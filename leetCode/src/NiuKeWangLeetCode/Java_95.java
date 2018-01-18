fca5642e1c1a964b10b8e28c37402ddc60f26ea2
BrookLiao <qjliaoxy@163.com>
2018-01-18 10:53:09 +0800

    第九十五题，A[i]表示在i处最多可以跳A[i]步，要求问从第一个元素（index=0）开始，能否达到数组的最后一个元素，这里认为最后一个元素为终点。超过也行

---
+++ leetCode/src/NiuKeWangLeetCode/Java_95.java
@@ -0,0 +1,25 @@
+package NiuKeWangLeetCode;
+
+/**
+ * Created by LXY on 2018/1/18.
+ */
+public class Java_95 {
+
+    //A[i]表示在i处最多可以跳A[i]步，要求问从第一个元素（index=0）开始，能否达到数组的最后一个元素，这里认为最后一个元素为终点。超过也行
+    public boolean canJump(int[] A) {
+        if(A.length<1)
+            return false;
+        if(A.length==1)
+            return true;
+        int max = 0;        //记录当前能到达的最大点
+        for(int i=0;i<A.length-1;i++){
+            max = Math.max(max,i+A[i]);
+            if(max < i+1){      //到达不了后面的点
+                return false;
+            }
+            if(max >= A.length-1)       //能到达的最大点直接超过最后一个，那么返回true
+                return true;
+        }
+        return false;
+    }
+}
